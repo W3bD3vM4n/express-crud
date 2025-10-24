323a66b78173feb6c05fced2e1078aea8f36f011
Hassan Rahman <hr-awesome@hotmail.com>
2025-10-24 11:57:21 -0500

    Fix dev script and related errors

--- src/entities/post.ts
+++ src/entities/post.ts
@@ -31,14 +31,14 @@ export class Post {
     updatedAt!: Date;
 
     // Relationships
-    @Column({ name: 'user_id', nullable: true })
+    @Column({ type: 'int', name: 'user_id', nullable: true })
     userId!: number | null;
 
     @ManyToOne('User', (user: User) => user.posts, { onDelete: 'SET NULL' })
     @JoinColumn({ name: 'user_id' })
     user!: User;
 
-    @Column({ name: 'category_id' })
+    @Column({ type: 'int', name: 'category_id' })
     categoryId!: number;
 
     @ManyToOne('Category', (category: any) => category.posts, { onDelete: 'RESTRICT' })
